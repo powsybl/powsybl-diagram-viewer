bc564ab90281de0ee63f90f83b3c55e48e58e861
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2024-01-29 14:48:34 +0100

    fix: PR remarks + react removal
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import eslint from 'vite-plugin-eslint';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-    plugins: [react(), eslint(), dts()],
+    plugins: [eslint(), dts()],
     build: {
         lib: {
             // Could also be a dictionary or array of multiple entry points
@@ -14,18 +13,6 @@ export default defineConfig({
             // the proper extensions will be added
             fileName: 'powsybl-diagram-viewer',
             formats: ['es'],
-        },
-        rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: ['react'],
-            output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                    react: 'React',
-                },
-            },
         },
     },
 });
