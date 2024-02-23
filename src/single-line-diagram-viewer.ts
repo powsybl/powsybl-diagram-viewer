f0b01d6a62358f0a56f1e1c8086c2901261dc44f
Florian Dupuy <66690739+flo-dup@users.noreply.github.com>
2024-02-23 15:39:40 +0100

    [sld-viewer] Center navigation arrow (#33)
    * Center navigation arrow
    * Rather fail with exception than silently fail with 0 width
    
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- src/single-line-diagram-viewer.ts
+++ src/single-line-diagram-viewer.ts
@@ -429,10 +429,14 @@ export class SingleLineDiagramViewer {
                     const xs = transform?.[0]?.match(/\d+/)?.[0];
                     if (xs !== undefined) {
                         const x = parseInt(xs, 10);
+                        const feederWidth =
+                            this.svgMetadata?.components.find(
+                                (comp) => comp.type === element.componentType
+                            ).size.width || 0;
                         this.createSvgArrow(
                             elementById,
                             element.direction,
-                            x,
+                            x + feederWidth / 2,
                             highestY.get(element.vid),
                             lowestY.get(element.vid)
                         );
