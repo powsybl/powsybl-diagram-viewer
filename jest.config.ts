e3b943d6ea9202815dae3d3cce7e58ef9328a0a9
Sylvain Bouzols <sylvain.bouzols@gmail.com>
2024-03-28 18:14:22 +0100

    Add github action to check license header and dependencies license (#51)
    * feat(): add github action for license headers and license checker dev dependency
    
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 /*
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/en/configuration.html
