8662b95a3d803e71af50304b066c865fa759ead9
sBouzols <sylvain.bouzols@gmail.com>
2024-03-28 17:34:48 +0100

    fix(): Add missing license headers
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import eslint from 'vite-plugin-eslint';
