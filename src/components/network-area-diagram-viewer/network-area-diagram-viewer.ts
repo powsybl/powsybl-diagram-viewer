18008114e95ca1f1a611688168d704a68f2ed1b5
Massimo Ferraro <massimo.ferraro@soft.it>
2024-08-02 17:03:34 +0200

    Allow to select nodes in network area diagram viewer (#90)
    Signed-off-by: massimo.ferraro <massimo.ferraro@soft.it>

--- src/components/network-area-diagram-viewer/network-area-diagram-viewer.ts
+++ src/components/network-area-diagram-viewer/network-area-diagram-viewer.ts
@@ -22,6 +22,11 @@ export type OnMoveNodeCallbackType = (
     yOrig: number
 ) => void;
 
+export type OnSelectNodeCallbackType = (
+    equipmentId: string,
+    nodeId: string
+) => void;
+
 export class NetworkAreaDiagramViewer {
     container: HTMLElement;
     svgContent: string;
@@ -37,7 +42,9 @@ export class NetworkAreaDiagramViewer {
     initialPosition: Point = new Point(0, 0);
     svgParameters: SvgParameters;
     edgeAngles: Map<string, number> = new Map<string, number>();
-    onNodeCallback: OnMoveNodeCallbackType | null;
+    onMoveNodeCallback: OnMoveNodeCallbackType | null;
+    onSelectNodeCallback: OnSelectNodeCallbackType | null;
+    shiftKeyOnMouseDown: boolean = false;
 
     constructor(
         container: HTMLElement,
@@ -46,7 +53,8 @@ export class NetworkAreaDiagramViewer {
         minHeight: number,
         maxWidth: number,
         maxHeight: number,
-        onNodeCallback: OnMoveNodeCallbackType | null,
+        onMoveNodeCallback: OnMoveNodeCallbackType | null,
+        onSelectNodeCallback: OnSelectNodeCallbackType | null,
         enableNodeMoving: boolean
     ) {
         this.container = container;
@@ -57,7 +65,8 @@ export class NetworkAreaDiagramViewer {
         this.originalHeight = 0;
         this.init(minWidth, minHeight, maxWidth, maxHeight, enableNodeMoving);
         this.svgParameters = this.getSvgParameters();
-        this.onNodeCallback = onNodeCallback;
+        this.onMoveNodeCallback = onMoveNodeCallback;
+        this.onSelectNodeCallback = onSelectNodeCallback;
     }
 
     public setWidth(width: number): void {
@@ -263,36 +272,62 @@ export class NetworkAreaDiagramViewer {
         if (!draggableElem) {
             return;
         }
-        this.disablePanzoom(); // to avoid panning the whole SVG when moving a node
-        this.ctm = this.svgDraw?.node.getScreenCTM(); // used to compute mouse movement
-        this.selectedElement = draggableElem as SVGGraphicsElement; // element to be moved
+        this.disablePanzoom(); // to avoid panning the whole SVG when moving or selecting a node
+        this.selectedElement = draggableElem as SVGGraphicsElement; // element to be moved or selected
+        // change cursor style
         const svg: HTMLElement = <HTMLElement>(
             this.svgDraw?.node.firstElementChild?.parentElement
         );
         if (svg != null) {
             svg.style.cursor = 'grabbing';
         }
-        this.initialPosition = DiagramUtils.getPosition(this.selectedElement); // used for the offset
-        this.edgeAngles = new Map<string, number>();
+        if (!(event as MouseEvent).shiftKey) {
+            // moving node
+            this.shiftKeyOnMouseDown = false;
+            this.ctm = this.svgDraw?.node.getScreenCTM(); // used to compute mouse movement
+            this.initialPosition = DiagramUtils.getPosition(
+                this.selectedElement
+            ); // used for the offset
+            this.edgeAngles = new Map<string, number>();
+        } else {
+            // selecting node
+            this.shiftKeyOnMouseDown = true;
+        }
     }
 
     private drag(event: Event) {
         if (this.selectedElement) {
             event.preventDefault();
-            this.ctm = this.svgDraw?.node.getScreenCTM();
-            const mousePosition = this.getMousePosition(event as MouseEvent);
-            this.updateGraph(mousePosition);
-            this.initialPosition = DiagramUtils.getPosition(
-                this.selectedElement
-            );
+            if (!this.shiftKeyOnMouseDown) {
+                // moving node
+                this.ctm = this.svgDraw?.node.getScreenCTM(); // used to compute mouse movement
+                const mousePosition = this.getMousePosition(
+                    event as MouseEvent
+                );
+                this.updateGraph(mousePosition);
+                this.initialPosition = DiagramUtils.getPosition(
+                    this.selectedElement
+                );
+            }
         }
     }
 
     private endDrag(event: Event) {
         if (this.selectedElement) {
-            const mousePosition = this.getMousePosition(event as MouseEvent);
-            this.updateGraph(mousePosition);
-            this.updateMetadataCallCallback(mousePosition);
+            if (!this.shiftKeyOnMouseDown) {
+                // moving node
+                const mousePosition = this.getMousePosition(
+                    event as MouseEvent
+                );
+                this.updateGraph(mousePosition);
+                this.updateMetadataCallCallback(mousePosition);
+                this.initialPosition = new Point(0, 0);
+            } else {
+                // selecting node
+                this.callSelectNodeCallback();
+                this.shiftKeyOnMouseDown = false;
+            }
+            // change cursor style
             const svg: HTMLElement = <HTMLElement>(
                 this.svgDraw?.node.firstElementChild?.parentElement
             );
@@ -300,7 +335,6 @@ export class NetworkAreaDiagramViewer {
                 svg.style.removeProperty('cursor');
             }
             this.selectedElement = null;
-            this.initialPosition = new Point(0, 0);
             this.enablePanzoom();
         }
     }
@@ -1276,15 +1310,32 @@ export class NetworkAreaDiagramViewer {
             // update node position in metadata
             node.setAttribute('x', xNew);
             node.setAttribute('y', yNew);
-            // call the callback, if defined
-            if (this.onNodeCallback != null) {
-                this.onNodeCallback(
+            // call the move node callback, if defined
+            if (this.onMoveNodeCallback != null) {
+                this.onMoveNodeCallback(
                     node.getAttribute('equipmentid') ?? '',
                     node.getAttribute('svgid') ?? '',
                     +xNew,
                     +yNew,
                     +xOrig,
                     +yOrig
+                );
+            }
+        }
+    }
+
+    private callSelectNodeCallback() {
+        // call the select node callback, if defined
+        if (this.onSelectNodeCallback != null) {
+            // get selected node from metadata
+            const node: SVGGraphicsElement | null =
+                this.container.querySelector(
+                    'nad\\:node[svgid="' + this.selectedElement?.id + '"]'
+                );
+            if (node != null) {
+                this.onSelectNodeCallback(
+                    node.getAttribute('equipmentid') ?? '',
+                    node.getAttribute('svgid') ?? ''
                 );
             }
         }
