c3823b73841257ad8e12df06e42c031e6937a253
Florian Dupuy <florian.dupuy@rte-france.com>
2024-11-05 10:48:10 +0100

    Fix redraw when boundary node moved
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- src/components/network-area-diagram-viewer/network-area-diagram-viewer.ts
+++ src/components/network-area-diagram-viewer/network-area-diagram-viewer.ts
@@ -765,8 +765,8 @@ export class NetworkAreaDiagramViewer {
         // if dangling line edge -> redraw boundary node
         if (edgeType == DiagramUtils.EdgeType.DANGLING_LINE) {
             this.redrawBoundaryNode(edgeNodes[1], DiagramUtils.getAngle(edgeStart2, edgeMiddle), nodeRadius2[1]);
-            if (this.selectedElement?.id == edgeNodes[1]?.id) {
-                // if boudary node moved -> redraw other voltage level node
+            if (this.draggedElement?.id == edgeNodes[1]?.id) {
+                // if boundary node moved -> redraw other voltage level node
                 this.redrawOtherVoltageLevelNode(edgeNodes[0], [edge]);
             }
         } else {
