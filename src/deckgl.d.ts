dacfc1c12ffce77abc6b19fc7c018ce6ad877f37
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2024-11-13 11:06:15 +0100

    Resolve TypeScript imports

--- src/deckgl.d.ts
+++ src/deckgl.d.ts
@@ -8,9 +8,27 @@
 /* eslint-disable */
 
 /* Override for v8 following
- * https://deck.gl/docs/get-started/using-with-typescript
+ * https://deck.gl/docs/get-started/using-with-typescript#deckgl-v8
+ * TODO: remove this file when migrating to deck.gl v9
  */
 declare module 'deck.gl' {
     //export namespace DeckTypings {}
     export * from 'deck.gl/typed';
 }
+declare module '@deck.gl/aggregation-layers' { export * from '@deck.gl/aggregation-layers/typed'; }
+declare module '@deck.gl/carto' { export * from '@deck.gl/carto/typed'; }
+declare module '@deck.gl/core' { export * from '@deck.gl/core/typed'; }
+declare module '@deck.gl/extensions' { export * from '@deck.gl/extensions/typed'; }
+declare module '@deck.gl/geo-layers' { export * from '@deck.gl/geo-layers/typed'; }
+declare module '@deck.gl/google-maps' { export * from '@deck.gl/google-maps/typed'; }
+declare module '@deck.gl/json' { export * from '@deck.gl/json/typed'; }
+declare module '@deck.gl/layers' { export * from '@deck.gl/layers/typed'; }
+declare module '@deck.gl/mapbox' { export * from '@deck.gl/mapbox/typed'; }
+declare module '@deck.gl/mesh-layers' { export * from '@deck.gl/mesh-layers/typed'; }
+declare module '@deck.gl/react' { export * from '@deck.gl/react/typed'; }
+
+/* For @luma.gl v8, the best would be to use @danmarshall/deckgl-typings work, but it conflicts with "@deck.gl/<module>/typed"...
+ * Has we will migrate to deck.gl v9 very soon, it's acceptable to just let typescript not check types temporally.
+ * TODO: remove this file when migrating to deck.gl v9
+ */
+declare module '@luma.gl/core'; // just shut down tsc with 'any'
